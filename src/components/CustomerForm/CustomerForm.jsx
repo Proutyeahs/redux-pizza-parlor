470791fb03474689f3569f374458664e7b13d6e9
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-18 10:31:42 -0500

    basic form set up

---
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -0,0 +1,17 @@
+
+function CustomerForm () {
+
+    return (
+        <>
+            <input type='text' placeholder="Name"/>
+            <input type='text' placeholder="StreetAddress"/>
+            <input type='text' placeholder="City"/>
+            <input type='text' placeholder="Zip"/>
+            <input type="checkbox" placeholder="Pickip"/>
+            <input type="checkbox" placeholder="Delivery"/>
+            <button>Next</button>
+        </>
+    )
+}
+
+export default CustomerForm
