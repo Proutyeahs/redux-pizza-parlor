d419e3f304f55369fc9554899cf7d467303712f8
Matt Moua <matmoua@gmail.com>
2022-08-19 13:02:50 -0500

    fixed errors

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -2,7 +2,6 @@ import PizzaItem from '../PizzaItem/PizzaItem.jsx';
 import {useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { Button } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
 
 
 import './PizzaList.css';
@@ -31,7 +30,6 @@ function PizzaList () {
             )
         })}
 
-        <div className="floatingTotal">TOTAL: {totalPrice}</div>
          <Button 
             className='nextButton'
             onClick={nextPage}
